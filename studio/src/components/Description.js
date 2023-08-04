7e7236f3ecc754c0dcafb12aea9951c3142c597b
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-03 19:58:02 -0500

    second commit react studio

--- studio/src/components/Description.js
+++ studio/src/components/Description.js
@@ -1,16 +1,17 @@
-function RecipeAuthor = () => {
-    const authorLink = "https://www.allthingsmamma.com/buffalo-chicken-dip-recipe-perfect-for-the-crock-pot-or-oven/"
-    const authorPhoto = "https://allthingsmamma.com/wp-content/uploads/2020/01/Kasey-Schwartz-webprofile.jpg"
-    const authorName = "Kasey Schwartz"
+function RecipeAuthor () {
+    const authorLink =
+        "https://www.allthingsmamma.com/buffalo-chicken-dip-recipe-perfect-for-the-crock-pot-or-oven/";
+    const authorPhoto =
+        "https://allthingsmamma.com/wp-content/uploads/2020/01/Kasey-Schwartz-webprofile.jpg";
+    const authorName = "Kasey Schwartz";
 
     return (
-        <div className={StyleSheet.recipeAuthorBlock}>
-            <img src={authorPhoto} alt="Author" className={styels.imageUpdates} />
+        <div className={styles.recipeAuthorBlock}>
+            <img src={authorPhoto} alt="Author" className={styles.imageUpdates} />
             <div>
                 <h3>{authorName}</h3>
                 <a href={authorLink}>Blog name</a>
+            </div>
         </div>
-        </div >
     );
-
-}
+};
